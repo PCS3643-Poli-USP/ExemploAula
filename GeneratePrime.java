21782dd93d34184ff09306568a224650c9622b7a
Max <max.rosan@ymail.com>
2016-08-31 20:39:22 -0300

    Primeira versão do projeto.

---
+++ GeneratePrime.java
@@ -0,0 +1,31 @@
+
+public class GeneratePrime {
+
+	private boolean isPrimeMethod(int i) {
+		int j;
+		boolean isPrime = true;
+
+		for (j = 2; isPrime && j < i; j++) {
+			if ((i % j) == 0) {
+				isPrime = false;
+			}
+		}
+
+		return isPrime;
+	}
+
+	public static void main(String[] args) {
+
+        int i, j;
+        boolean isPrime;
+        
+        System.out.println("Generating primes...");
+        
+        for (i = 2; i < 1000; i++) {
+            if (isPrimeMethod(i)) {
+            	System.out.println(i + " is prime.");
+            }
+        }
+	}
+
+}
